8d423e4b4e2109f65a03b698ebbb5115f95806db
Denis Shurygin <sdi.linch@gmail.com>
2018-07-15 13:37:03 +0300

    Test TextUtils in corespondet Test class

--- platform-mobile/src/test/java/ru/pocketbyte/locolaser/platform/mobile/resource/file/IosSwiftResourceFileTest.java
+++ platform-mobile/src/test/java/ru/pocketbyte/locolaser/platform/mobile/resource/file/IosSwiftResourceFileTest.java
@@ -31,16 +31,6 @@ public class IosSwiftResourceFileTest {
     public TemporaryFolder tempFolder= new TemporaryFolder();
 
     @Test
-    public void testKeyToProperty() {
-        assertEquals("some_value", TextUtils.keyToProperty("Some Value"));
-        assertEquals("some_value", TextUtils.keyToProperty("some__value__"));
-        assertEquals("some_value2", TextUtils.keyToProperty("some 'value2'"));
-        assertEquals("some_value_2", TextUtils.keyToProperty("some 'value' + 2"));
-        assertEquals("formula_x_y_3_2_z", TextUtils.keyToProperty("formula x = (y + 3/2) * z."));
-        assertEquals("_1_value", TextUtils.keyToProperty("1 value"));
-    }
-
-    @Test
     public void testRead() throws IOException {
         IosSwiftResourceFile resourceFile = new IosSwiftResourceFile(tempFolder.newFile(), "Strings", "Strings");
         assertNull(resourceFile.read());
