ef6a4573e28e39e0c42f1a578dc0448b7f8a70c9
Denis Shurygin <sdi.linch@gmail.com>
2019-10-23 23:19:40 +0300

    Move part of Main class code into separate class ConfigParserFactory

---
+++ core/src/main/java/ru/pocketbyte/locolaser/ConfigParserFactory.kt
@@ -0,0 +1,111 @@
+package ru.pocketbyte.locolaser
+
+import ru.pocketbyte.locolaser.config.parser.*
+import sun.misc.Unsafe
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.JarURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import java.net.URLClassLoader
+import java.util.ArrayList
+import java.util.LinkedHashSet
+
+class ConfigParserFactory {
+
+    @Throws(Exception::class)
+    fun get(): ConfigParser {
+        return getFromClassLoader()
+    }
+
+    @Throws(ClassNotFoundException::class, IllegalAccessException::class, InstantiationException::class, IOException::class)
+    private fun getFromClassLoader(): ConfigParser {
+        val platformConfigParsers = LinkedHashSet<PlatformConfigParser<*>>()
+        val sourceConfigParsers = LinkedHashSet<SourceConfigParser<*>>()
+
+        for (url in jarUrls) {
+            readPlatformConfigParsers(url)
+                    .mapTo(platformConfigParsers) {
+                        Class.forName(it).newInstance() as PlatformConfigParser<*>
+                    }
+
+            readSourceConfigParsers(url)
+                    .mapTo(sourceConfigParsers) {
+                        Class.forName(it).newInstance() as SourceConfigParser<*>
+                    }
+        }
+
+        if (platformConfigParsers.size == 0)
+            throw RuntimeException("Platform Config parser not found")
+
+        sourceConfigParsers.add(EmptySourceConfigParser())
+
+        return ConfigParser(
+                SourceSetConfigParser(sourceConfigParsers),
+                PlatformSetConfigParser(platformConfigParsers))
+    }
+
+    // jdk9
+    // jdk.internal.loader.ClassLoaders.AppClassLoader.ucp
+    // jdk.internal.loader.URLClassPath.path
+    private val jarUrls: Array<URL>
+        get() {
+            val classLoader = Main::class.java.classLoader ?: throw RuntimeException("ClassLoader is null")
+
+            if (classLoader is URLClassLoader)
+                return classLoader.urLs
+            if (classLoader.javaClass.name.startsWith("jdk.internal.loader.ClassLoaders$")) {
+                try {
+                    val fieldUnsafe = Unsafe::class.java.getDeclaredField("theUnsafe")
+                    fieldUnsafe.isAccessible = true
+                    val unsafe = fieldUnsafe.get(null) as Unsafe
+                    val ucpField = classLoader.javaClass.getDeclaredField("ucp")
+                    val ucpFieldOffset = unsafe.objectFieldOffset(ucpField)
+                    val ucpObject = unsafe.getObject(classLoader, ucpFieldOffset)
+                    val pathField = ucpField.type.getDeclaredField("path")
+                    val pathFieldOffset = unsafe.objectFieldOffset(pathField)
+                    val path = unsafe.getObject(ucpObject, pathFieldOffset) as ArrayList<URL>
+
+                    return path.toTypedArray()
+                } catch (e: Exception) {
+                    throw RuntimeException("Failed to read list of execution jars", e)
+                }
+
+            }
+
+            throw RuntimeException("Failed to read list of execution jars")
+        }
+
+    private fun readPlatformConfigParsers(jarUrl: URL): List<String> {
+        return readLines(jarUrl, "/META-INF/platform_configs")
+    }
+
+    private fun readSourceConfigParsers(jarUrl: URL): List<String> {
+        return readLines(jarUrl, "/META-INF/source_configs")
+    }
+
+    private fun readLines(jarUrl: URL, filePath: String): List<String> {
+        val lines = mutableListOf<String>()
+
+        val inputFilePath = "jar:file:${jarUrl.path}!$filePath"
+
+        var reader: BufferedReader? = null
+        try {
+            val inputURL = URL(inputFilePath)
+            val conn = inputURL.openConnection() as JarURLConnection
+            reader = BufferedReader(InputStreamReader(conn.inputStream))
+
+            reader.lines().forEach { lines.add(it) }
+        } catch (e: MalformedURLException) {
+            System.err.println("Malformed input URL: $inputFilePath")
+        } catch (e: IOException) {
+            // Do nothing
+        }
+        finally {
+            reader?.close()
+        }
+
+        return lines
+    }
+}
